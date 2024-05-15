51686820f564cec5a095ddba63e66a83da0c78f7
ndz <nicolas.dzierzgowski@gmail.com>
2024-05-15 11:09:05 +0200

    working on cors

--- front-end/src/app/interceptors/auth.interceptor.ts
+++ front-end/src/app/interceptors/auth.interceptor.ts
@@ -2,29 +2,29 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtToken = getJwtToken();
-  if(jwtToken){
+  if (jwtToken) {
+    console.log('set access control');
     let cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${jwtToken}`,
-        'Content-Type':'application/json',
-        'Access-Control-Allow-Origin':"*"
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
     });
     return next(cloned);
-
-  }else{
+  } else {
+    console.log('set access control');
     let cloned = req.clone({
       setHeaders: {
-        'Content-Type':'application/json',
-        'Access-Control-Allow-Origin':" https://blogdbhazar-nico-5d30f5ae698b.herokuapp.com "
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': ' * ',
       },
     });
     return next(cloned);
   }
-  
 };
 
-function getJwtToken ():string | null {
-  console.log("getting jwt")
+function getJwtToken(): string | null {
+
   return localStorage.getItem('JWT_TOKEN');
 }
